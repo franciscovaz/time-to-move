2b1147aaff1049d23b75ce3bee44377704d07d3e
Francisco Vaz <ftomasvaz@gmail.com>
2021-02-23 19:07:36 +0000

    create profile component create home css

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/franciscovaz.png" alt="Francisco Vaz"/>
+      <div>
+        <strong>Francisco Vaz</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
