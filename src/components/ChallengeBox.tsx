4bcfdc572b28325fe6b1e486a19d9db6172975a1
Francisco Vaz <ftomasvaz@gmail.com>
2021-02-24 21:34:22 +0000

    challenge active style

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,43 @@
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox () {
+  const hasActiveChallenge = true;
   return(
     <div className={styles.challengeBoxContainer}>
-      <div className={styles.challengeNotActive}>
+      { hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header> Win 400 xp</header>
+
+          <main>
+            <img src="icons/body.svg"/>
+            <strong>New challenge</strong>
+            <p>Get up and do a 3 minute walk</p>
+          </main>
+
+          <footer>
+            <button 
+              type="button"
+              className={styles.challengeFailedButton}
+            >
+              Failed
+            </button>
+            <button 
+              type="button"
+              className={styles.challengeSucceededButton}
+            >
+              Completed
+            </button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
         <strong>Finish one cycle to receive a challenge</strong>
         <p>
           <img src="icons/level-up.svg" alt="Level up"/>
           Level up by completing challenges.
         </p>
       </div>
+      )}
     </div>
   )
 }
