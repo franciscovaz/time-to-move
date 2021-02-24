28066cf3e19c7251ee81a200cc13b172f674d846
Francisco Vaz <ftomasvaz@gmail.com>
2021-02-24 23:06:50 +0000

    failed challenge -> call resetChallenge context function

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -14,6 +14,7 @@ interface ChallengesContextData {
   activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -42,6 +43,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     setActiveChallenge(challenge);
   }
 
+  function resetChallenge() {
+    setActiveChallenge(null);
+  }
+
   return(
     <ChallengesContext.Provider value={{
       level, 
@@ -49,7 +54,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
       challengesCompleted, 
       activeChallenge,
       levelUp,
-      startNewChallenge
+      startNewChallenge,
+      resetChallenge
       }}
     >
       {children}
