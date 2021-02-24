ab7700c6a5db686b99a79adc4e8a1c8f0010dd11
Francisco Vaz <ftomasvaz@gmail.com>
2021-02-24 20:53:30 +0000

    show closed cycle button when cycle ends

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -5,8 +5,9 @@ let countdownTimeout: NodeJS.Timeout;
 
 
 export function Countdown() {
-  const [time, setTime] = useState(25 * 60);
+  const [time, setTime] = useState(0.1 * 60);
   const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -23,7 +24,7 @@ export function Countdown() {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(25 * 60);
+    setTime(0.1 * 60);
   }
 
   useEffect(() => {
@@ -31,12 +32,15 @@ export function Countdown() {
       countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000);
+    } else if(isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
     }
   }, [isActive, time]);
 
   return(
     <div>
-<div className={styles.countdownContainer}>
+      <div className={styles.countdownContainer}>
       <div>
         <span>{minuteLeft}</span>
         <span>{minuteRight}</span>
@@ -48,7 +52,17 @@ export function Countdown() {
       </div>
     </div>
 
-    { isActive ? (
+    { hasFinished ? (
+      <button 
+        disabled
+        className={styles.countdownButton} 
+      >
+       Closed cycle
+        </button>
+    ) : 
+    (
+      <>
+      { isActive ? (
       <button type="button" 
       className={`${styles.countdownButton} ${styles.countdownButtonActive}`} 
       onClick={resetCountdown}>
@@ -60,6 +74,12 @@ export function Countdown() {
        Start cycle
         </button>
     )}
+      </>
+    )
+  
+  }
+
+    
 
     
     </div>
